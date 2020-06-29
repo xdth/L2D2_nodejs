5a807e653d5c64a29440daa1664d0eee18174eeb
dth <dth@dthlabs.com>
2020-06-30 01:09:51 +0200

    finished

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,8 @@ import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 export default async (name = 'default'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  console.log(defaultOptions.database);
+
   return createConnection(
     Object.assign(defaultOptions, {
       name,
